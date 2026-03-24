16027eb94643de03cfcf5d9a78cbd21f1a7c16c1
WaterSense <watersense@project.fr>
2026-03-24 11:53:52 +0100

    chore: suppression section partenaires/placeholder logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { Header }             from '@/components/landing/Header'
 import { HeroSection }        from '@/components/landing/HeroSection'
 import { ROICalculator }      from '@/components/landing/ROICalculator'
 import { FeaturesSection }    from '@/components/landing/FeaturesSection'
-import { SocialProofSection } from '@/components/landing/SocialProofSection'
 import { PricingSection }     from '@/components/landing/PricingSection'
 import { ContactSection }     from '@/components/landing/ContactSection'
 import { CtaSection }         from '@/components/landing/CtaSection'
@@ -17,7 +16,6 @@ export default function Home() {
       <HeroSection />
       <ROICalculator />
       <FeaturesSection />
-      <SocialProofSection />
       <PricingSection />
       <ContactSection />
       <CtaSection />
